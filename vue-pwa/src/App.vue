606d20e7a5d2768ec18bae868bd3353709956199
Tim Aro <timaro@live.se>
2019-11-07 12:25:44 +0100

    small restructure before new packages

--- vue-pwa/src/App.vue
+++ vue-pwa/src/App.vue
@@ -4,25 +4,5 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
-
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
-</script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script lang="ts" src="./App.ts"></script>
+<style lang="sass" src="./App.sass"></style>
